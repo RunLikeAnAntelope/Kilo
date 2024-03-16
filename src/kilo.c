1a9dff803475b15fbd0752ccf95861af0defc3d8
Nevin Leh <nevinleh@proton.me>
2024-03-16 17:21:24 -0600

    Up to step 32

--- src/kilo.c
+++ src/kilo.c
@@ -1,6 +1,6 @@
-/* Kilo -- A very simple editor in less than 1-kilo lines of code (as counted
- *         by "cloc"). Does not depend on libcurses, directly emits VT100
- *         escapes on the terminal.
+/* Kilo minus -- A very simple editor based on the kilo test editor made by
+ * Salvatore Sanfilippo. Based on instruction from
+ * https://viewsourcecode.org/snaptoken/kilo/
  *
  * -----------------------------------------------------------------------
  *
@@ -127,10 +127,40 @@ char editorReadKey() {
     return c;
 }
 
+int getCursorPosition(int* rows, int* cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
+        return -1;
+    }
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) {
+            break;
+        }
+        if (buf[i] == 'R') {
+            break;
+        }
+        i++;
+    }
+
+    buf[i] = '\0';
+
+    printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
+
+    editorReadKey();
+
+    return -1;
+}
+
 int getWindowSize(int* rows, int* cols) {
     struct winsize ws;
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        return -1;
+    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
+            return -1;
+        }
+        return getCursorPosition(rows, cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
