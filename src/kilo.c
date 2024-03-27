4098cd2093e00cae58cec7c3ed0e51a241a27eff
Nevin Leh <nevinleh@proton.me>
2024-03-26 23:07:25 -0600

    Done through 127

--- src/kilo.c
+++ src/kilo.c
@@ -58,7 +58,7 @@
 
 /*+++ defines +++*/
 #define KILO_VERSION "0.0.1"
-#define KILO_TAB_STOP 8
+#define KILO_TAB_STOP 4
 #define KILO_QUIT_TIMES 3
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -112,6 +112,8 @@ void die(const char* s) {
 /*+++ prototypes ++*/
 
 void editorSetStatusMessage(const char* fmt, ...);
+void editorRefreshScreen();
+char* editorPrompt(char* prompt);
 
 // restore original mode when we exit
 void disableRawMode() {
@@ -467,7 +469,7 @@ void editorOpen(char* filename) {
 
 void editorSave() {
     if (E.filename == NULL) {
-        return;
+        E.filename = editorPrompt("Save as: %s");
     }
 
     int len;
@@ -514,6 +516,34 @@ void abAppend(struct abuf* ab, const char* s, int len) {
 
 void abFree(struct abuf* ab) { free(ab->b); }
 /*+++ input +++*/
+char* editorPrompt(char* prompt) {
+    size_t bufsize = 128;
+    char* buf = malloc(bufsize);
+
+    size_t buflen = 0;
+    buf[0] = '\0';
+
+    while (1) {
+        editorSetStatusMessage(prompt, buf);
+        editorRefreshScreen();
+
+        int c = editorReadKey();
+        if (c == '\r') {
+            if (buflen != 0) {
+                editorSetStatusMessage("");
+                return buf;
+            }
+        } else if (!iscntrl(c) && c < 128) {
+            if (buflen == bufsize - 1) {
+                bufsize *= 2;
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
+    }
+}
+
 void editorMoveCursor(int key) {
     erow* row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
     switch (key) {
